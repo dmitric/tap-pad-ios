5686f2a756b6e94b56357fd89705235eb2dc1199
Dmitri Cherniak <dmitric@gmail.com>
2013-08-13 14:09:12 -0400

    change grid load scheme

--- TapPad/AppDelegate.m
+++ TapPad/AppDelegate.m
@@ -15,7 +15,6 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
     self.viewController = [[TapPadViewController alloc]
                            initWithNibName:@"TapPadViewController" bundle:nil];
     self.window.rootViewController = self.viewController;
@@ -25,19 +24,27 @@
 
 - (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url
 {
-    if (url.host) {
-        [self.viewController loadSound:url.host];
-    }
+    [self handleOpenUrl:url];
     return YES;
 }
 
 - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
     sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
 {
-    if (url.host) {
-        [self.viewController loadSound:url.host];
-    }
+    [self handleOpenUrl:url];
     return YES;
+}
+
+-(void)handleOpenUrl:(NSURL *)url
+{
+    NSString *action = [url.host lowercaseString];
+    if (action) {
+        if ([action isEqualToString:@"grid"]
+            && url.pathComponents.count > 1){
+            NSString *code = url.pathComponents[1];
+            [self.viewController loadSound:code];
+        }
+    }
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
